5de07b9d805457552b2203781a85ad8cd1d06a70
Muzamil Junaid <55745028+MuzamilJunaid@users.noreply.github.com>
2019-10-17 18:04:00 +0500

    Add files via upload

---
+++ Mavenjava/src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAPpAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");	
+	}
+}
